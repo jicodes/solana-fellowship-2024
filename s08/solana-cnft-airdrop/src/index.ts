56046a95811b6eb285cfb798d3cb2bb091e4be5d
jicodes <122512755+jicodes@users.noreply.github.com>
2024-09-10 23:15:27 -0700

    feat: execute cNFT creation and distribution

--- s08/solana-cnft-airdrop/src/index.ts
+++ s08/solana-cnft-airdrop/src/index.ts
@@ -1,1 +1,22 @@
-import { umi } from "./umiProvider";
+import {
+  createNftCollection,
+  createBubblegumTree,
+  mintCnfts,
+} from "./createNft";
+import { publicKey } from "@metaplex-foundation/umi";
+
+// wallet list to Airdrop cNFTs
+const walletAddresses = [
+  "wallet1",
+  "wallet2",
+  "wallet3", 
+].map((address) => publicKey(address));
+
+async function main() {
+  const collectionMint = await createNftCollection();
+  const merkleTree = await createBubblegumTree();
+
+  await mintCnfts(walletAddresses, merkleTree, collectionMint);
+}
+
+main().catch(console.error);
